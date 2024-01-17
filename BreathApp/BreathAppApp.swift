dec29e3219bd69e252d6abbb190ff95213c22d4a
Vladimir Vinakheras <vvinageras@gmail.com>
2024-01-17 02:55:42 +0200

    Initial Commit

---
+++ BreathApp/BreathAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreathAppApp.swift
+//  BreathApp
+//
+//  Created by Vladimir Vinakheras on 17.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct BreathAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
