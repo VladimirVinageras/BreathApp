efe0d0d89b9dff218570fc6dc9c46f1325162a13
Vladimir Vinakheras <vvinageras@gmail.com>
2024-01-20 02:30:00 +0300

    Breath app

--- BreathApp/ContentView.swift
+++ BreathApp/ContentView.swift
@@ -8,14 +8,61 @@
 import SwiftUI
 
 struct ContentView: View {
+    //animations variables
+    @State var scaleUpDown = true
+    @State var rotationInOut = false
+    @State var moveInOut = true
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            //Background
+            Rectangle()
+                .foregroundColor(.black)
+                .edgesIgnoringSafeArea(.all)
+            //Circles
+            ZStack{
+                ZStack{
+                    Circle().fill(LinearGradient(gradient: Gradient(colors: [.green,.white]), startPoint: .top, endPoint: .bottom))
+                        .frame(width: 120, height: 120, alignment: .center)
+                        .offset(y: moveInOut ? -60 : 0)
+                    
+                    Circle().fill(LinearGradient(gradient: Gradient(colors: [.green,.white]), startPoint: .bottom, endPoint: .top))
+                        .frame(width: 120, height: 120, alignment: .center)
+                        .offset(y: moveInOut ? 60 : 0)
+                }.opacity(0.5)
+                
+                ZStack{
+                    Circle().fill(LinearGradient(gradient: Gradient(colors: [.green,.white]), startPoint: .top, endPoint: .bottom))
+                        .frame(width: 120, height: 120, alignment: .center)
+                        .offset(y: moveInOut ? -60 : 0)
+                    
+                    Circle().fill(LinearGradient(gradient: Gradient(colors: [.green,.white]), startPoint: .bottom, endPoint: .top))
+                        .frame(width: 120, height: 120, alignment: .center)
+                        .offset(y: moveInOut ? 60 : 0)
+                }.opacity(0.5)
+                    .rotationEffect(.degrees(60))
+                
+                ZStack{
+                    Circle().fill(LinearGradient(gradient: Gradient(colors: [.green,.white]), startPoint: .top, endPoint: .bottom))
+                        .frame(width: 120, height: 120, alignment: .center)
+                        .offset(y: moveInOut ? -60 : 0)
+                    
+                    Circle().fill(LinearGradient(gradient: Gradient(colors: [.green,.white]), startPoint: .bottom, endPoint: .top))
+                        .frame(width: 120, height: 120, alignment: .center)
+                        .offset(y: moveInOut ? 60 : 0)
+                }.opacity(0.5)
+                    .rotationEffect(.degrees(120))
+            }
+            .rotationEffect(.degrees(rotationInOut ? 90 : 0 ))
+            .scaleEffect(scaleUpDown ? 1 : 1/4)
+            .animation(Animation.easeInOut.repeatForever(autoreverses: true).speed(1/8), value: moveInOut)
         }
-        .padding()
+        .onAppear(){
+            scaleUpDown.toggle()
+            rotationInOut.toggle()
+            moveInOut.toggle()
+            
+        }
     }
 }
 
